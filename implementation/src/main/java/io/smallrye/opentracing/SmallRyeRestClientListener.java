5f60657a759f427f1d7c6d2e2053a1d7c392e67d
Doychin Bondzhev <doychin@dsoft-bg.com>
2019-03-29 15:28:17 +0200

    Add warning message

--- implementation/src/main/java/io/smallrye/opentracing/SmallRyeRestClientListener.java
+++ implementation/src/main/java/io/smallrye/opentracing/SmallRyeRestClientListener.java
@@ -2,6 +2,8 @@ package io.smallrye.opentracing;
 
 import io.opentracing.Tracer;
 import javax.enterprise.inject.spi.CDI;
+import java.util.logging.Logger;
+
 import org.eclipse.microprofile.opentracing.Traced;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import org.eclipse.microprofile.rest.client.spi.RestClientListener;
@@ -10,6 +12,8 @@ import org.eclipse.microprofile.rest.client.spi.RestClientListener;
  * @author Pavol Loffay
  */
 public class SmallRyeRestClientListener implements RestClientListener {
+
+  private static final Logger logger = Logger.getLogger(SmallRyeRestClientListener.class.getName());
 
   @Override
   public void onNewClient(Class<?> clientInterface, RestClientBuilder restClientBuilder) {
@@ -23,6 +27,7 @@ public class SmallRyeRestClientListener implements RestClientListener {
     try {
       tracer = CDI.current().select(Tracer.class).get();
     } catch (IllegalStateException e) {
+      logger.warning("CDI container is unavailable");
       return;
     }
     restClientBuilder.register(new SmallRyeClientTracingFeature(tracer));
