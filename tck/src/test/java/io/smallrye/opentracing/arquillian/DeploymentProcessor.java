05c41d79c9220993a6de27bbd99695a03e90c391
Pavol Loffay <ploffay@redhat.com>
2020-07-10 16:18:46 +0200

    Bump MP-OpenTracing to 2.0 and OT to 0.33.0
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- tck/src/test/java/io/smallrye/opentracing/arquillian/DeploymentProcessor.java
+++ tck/src/test/java/io/smallrye/opentracing/arquillian/DeploymentProcessor.java
@@ -64,13 +64,11 @@ public class DeploymentProcessor implements ApplicationArchiveProcessor {
 
             String[] deps = {
                     "io.opentracing:opentracing-api",
-                    "com.fasterxml.jackson.jaxrs:jackson-jaxrs-json-provider",
                     "io.smallrye:smallrye-opentracing",
                     "org.jboss.resteasy:resteasy-client-microprofile",
                     "org.jboss.resteasy:resteasy-client",
+                    "org.jboss.resteasy:resteasy-json-binding-provider",
                     "org.jboss.resteasy:resteasy-cdi",
-                    "org.jboss.resteasy:resteasy-jaxb-provider",
-                    "org.jboss.resteasy:resteasy-jackson2-provider",
                     "org.jboss.weld.servlet:weld-servlet-core",
                     "io.smallrye.config:smallrye-config",
             };
